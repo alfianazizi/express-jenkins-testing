42f48be80b1819c21317f60b54983e1fbed4b6b3
alfianazizi <azizi@LAPTOP-3TV30BOC.localdomain>
2020-09-03 17:53:57 +0700

    change route index

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send(`hello world`);
+router.get('/', function (req, res, next) {
+  res.send(`hello world 2`);
 });
 
 module.exports = router;
