081f079934e1908842caabf9d834bfef9749ee99
alfianazizi <azizi@LAPTOP-3TV30BOC.localdomain>
2020-09-03 18:09:56 +0700

    change deployment method

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,6 +4,9 @@ ssh kirei@122.248.32.237 <<EOF
  cd ~/projects/express-jenkins-testing
  git pull
  npm install --production
- pm2 restart all
+ npm i -g pm2
+ export BUILD_ID=dontKillMePlease
+ pm2 startOrRestart ecosystem.config.js
+ pm2 save
  exit
 EOF
