8267e2a139280d3718ebcbb8fb1ded69836f23d8
alfianazizi <azizi@LAPTOP-3TV30BOC.localdomain>
2020-09-03 18:41:47 +0700

    test deploy

--- script/deploy.sh
+++ script/deploy.sh
@@ -6,7 +6,7 @@ ssh kirei@122.248.32.237 <<EOF
  pwd
  git pull
  npm install --production
- /home/kirei/.nvm/versions/node/v12.18.3/bin/pm2 start ./bin/www
+ /home/kirei/.nvm/versions/node/v12.18.3/bin/pm2 start /home/kirei/projects/express-jenkins-testing/bin/www
  /home/kirei/.nvm/versions/node/v12.18.3/bin/pm2 save
  exit
 EOF
